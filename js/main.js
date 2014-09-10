bbd41419e0d87e589aaaf6903a116a40a1f37651
kwadwo Nyarko <kwadwon@gmail.com>
2014-09-10 09:04:17 -0400

    updated javascript and index.html

--- js/main.js
+++ js/main.js
@@ -2,25 +2,29 @@ $(document).ready(function(){
 	$("#motto").click(function(event){
 		event.preventDefault();
 		//check on firefox and ie and chrome
-		$("html, body").animate({scrollTop:707}, 1000);
+		var offset = $("#c_motto").offset().top;
+		$("html, body").animate({scrollTop:offset-78}, 1000);
 	});
 
 	$("#healing").click(function(event){
 		event.preventDefault();
 		//check on firefox and ie and chrome
-		$("html, body").animate({scrollTop:1370}, 1000);
+		var offset = $("#c_healing").offset().top;
+		$("html, body").animate({scrollTop:offset-78}, 1000);
 	});
 
 	$("#teaching").click(function(event){
 		event.preventDefault();
 		//check on firefox and ie and chrome
-		$("html, body").animate({scrollTop:2120}, 1000);
+		var offset = $("#c_teaching").offset().top;
+		$("html, body").animate({scrollTop:offset-78}, 1000);
 	});
 
 	$("#preaching").click(function(event){
 		event.preventDefault();
 		//check on firefox and ie and chrome
-		$("html, body").animate({scrollTop:2845}, 1000);
+		var offset = $("#c_preaching").offset().top;
+		$("html, body").animate({scrollTop:offset-78}, 1000);
 	});
 
 	$("#btt").click(function(event){
